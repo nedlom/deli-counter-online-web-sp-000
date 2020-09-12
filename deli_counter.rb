659457717988d3b48a0bb52784158d954caab7ef
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-12 19:07:29 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def line(current_line)
+  if current_line == []
+    puts "The line is currently empty."
+  else
+    current_line.each_with_index do |customer, index|
+      current_line[index] = " #{index + 1}. #{customer}"
+    end
+    puts "The line is currently:#{current_line.join}"
+  end
+end
+
+def take_a_number(current_line,name)
+  current_line << name
+  puts "Welcome, #{name}. You are number #{current_line.length} in line."
+end
+
+def now_serving(current_line)
+  if current_line == []
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{current_line.shift}."
+  end
+end
